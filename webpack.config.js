3d2b9dc1965e2a33c4b3bcb9e1bdc8db7916abb4
Steve Mactaggart <steve@whitesquaresoft.com>
2018-11-26 23:32:26 +1100

    and another go

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist',
+    publicPath: '/',
     filename: 'build.js'
   },
   module: {
